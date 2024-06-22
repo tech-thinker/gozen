0e72d12a4bb7dc7afb43e414157dd7047e81ee38
Asif Mohammad Mollah <admin@mrasif.in>
2024-06-22 18:49:18 +0400

    exec shell functionality added in helper

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gozen
 Simplified Golang MVC code generator.
+
+## Installation
+Download and install executable binary from GitHub releases page.
+
+## `gozen` Guide
+- `gozen` help
+```sh
+gozen -h
+```
+
+- Create new project
+```sh
+gozen create -p github.com/<username>/<appname> <appname>
+```
--- cmd/helper/common.go
+++ cmd/helper/common.go
@@ -2,13 +2,18 @@ package helper
 
 import (
 	"embed"
+	"fmt"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/tech-thinker/gozen/utils"
 )
 
 type CommonHelper interface {
     Write(templatePath string, outputPath string, data interface{}) error
+    ExecShell(command string, args ...string) ([]string, error)
+    ExecShellRaw(command string, args ...string) ([]byte, error)
 }
 
 type commonHelper struct {
@@ -27,6 +32,31 @@ func (helper *commonHelper) Write(templatePath string, outputPath string, data i
         return err
     }
     return utils.WriteFile(outputPath, tpl)
+}
+
+// ExecShell: execute shell command and return output as string slice
+func (helper *commonHelper) ExecShell(command string, args ...string) ([]string, error) {
+    fmt.Printf(`%s %+v\n`, command, args)
+    cmd := exec.Command(command, args...)
+    output, err := cmd.Output()
+    if err != nil {
+        fmt.Println("Error executing command:", err)
+        return nil, err
+    }
+    lines := strings.Split(strings.TrimSpace(string(output)), "\n")
+    return lines, nil
+}
+
+// ExecShellRaw: execute shell command and return output as byte array
+func (helper *commonHelper) ExecShellRaw(command string, args ...string) ([]byte, error) {
+    fmt.Printf(`%s %+v\n`, command, args)
+    cmd := exec.Command(command, args...)
+    output, err := cmd.Output()
+    if err != nil {
+        fmt.Println("Error executing command:", err)
+        return nil, err
+    }
+    return output, nil
 }
 
 // NewCommonHelper returns a new CommonHelper
